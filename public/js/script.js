fb776e91e40870b1841a605afabebf30e4c8cb4c
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-10-15 16:30:45 +0700

    add: form user review

--- public/js/script.js
+++ public/js/script.js
@@ -13,21 +13,3 @@ email.addEventListener('keyup', (e) => {
     submitButton.style.cssText = 'background-color: #631465';
   }
 });
-
-// submitButton.disabled = true;
-// submitButton.style.cssText = 'width: 100%; background-color: #8e6d8f; color: #fff; padding: 6px 12px; border-radius: 8px; border: none;';
-
-// email.addEventListener('keyup', (e) => {
-//   if (email.value.length > 0) {
-//     password.addEventListener('keyup', (p) => {
-//       if (password.value !== '') {
-//         submitButton.disabled = false;
-//         submitButton.style.cssText = 'width: 100%; background-color: #631465; color: #fff; padding: 6px 12px; border-radius: 8px; border: none;';
-//       } else {
-//         button.disabled = true;
-//         submitButton.style.cssText = 'width: 100%; background-color: #8e6d8f; color: #fff; padding: 6px 12px; border-radius: 8px; border: none;';
-//       }
-//     });
-//   }
-// });
-
